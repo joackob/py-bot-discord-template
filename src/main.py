f4637da238c96f189f0f7d8907d509d02204fafc
Joaquin Blanco <blancojoaquin77@gmail.com>
2023-02-10 03:26:43 -0300

    refactor main.py: apply types

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import discord
 from discord import Intents
+from discord import Message
 import os
 
 
@@ -13,22 +14,22 @@ async def on_ready():
 
 
 @bot.event
-async def on_message(message):
+async def on_message(message: Message):
     if message.author == bot.user:
         return
 
-    mensaje = str(message.content)
-    autor = str(message.author)
+    mensaje = message.content
+    autor = message.author
 
     print(f'Escuche el mensaje: "{mensaje}" de {autor}')
 
     if 'saluda' in mensaje:
         await message.channel.send('Como va gentee @everyone')
-
     elif 'duerme' in mensaje:
         await message.channel.send('ok....zzzzz')
         await bot.close()
-
+    elif 'comandos' in mensaje:
+        await message.channel.send('Por el momento solo puedo saludar con "saluda" y dormir con "duerme"')
     else:
         await message.channel.send('jejeje no entendi')
 
